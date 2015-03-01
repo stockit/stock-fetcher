a536093d36cb95c89bc8e0162e75b6a55262c7ff
johnmcconnell <johnnyillinois@gmail>
2015-02-28 18:47:28 -0600

    updating the gitignore

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "stockit"
+
+version := "1.0"
+
+scalaVersion := "2.11.5"
+    
---
+++ src/main/scala/com/stockit/Stockit.scala
@@ -0,0 +1,8 @@
+package com.stockit
+
+/**
+ * Created by jmcconnell1 on 2/28/15.
+ */
+object Stockit extends App {
+  Console.println("Hello World!")
+}
