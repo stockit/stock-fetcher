8ad562c1823fdc76fcca80f572c29fa5c1f40768
johnmcconnell <johnnyillinois@gmail.com>
2015-03-22 16:48:16 -0500

    removing unneed exit syntax and add command line argument handling

--- src/main/scala/com/stockit/Stockit.scala
+++ src/main/scala/com/stockit/Stockit.scala
@@ -5,20 +5,23 @@ import java.nio.charset.StandardCharsets
  * Created by jmcconnell1 on 2/28/15.
  */
 object Stockit extends App {
-  println("Command format: symbol1,symbol2,...,startDate,endDate,outdir")
-  println("Example: YHOO,2009-10-14,2009-11-14,data")
-
   var client = yql.StockClient
 
-  var ok = true
+  if (args.length > 0) {
+    runQuery(args(0))
+  } else {
+    println("Command format: symbol1,symbol2,...,startDate,endDate,outdir")
+    println("Example: YHOO,2009-10-14,2009-11-14,data")
 
-  while (ok) {
-    val ln = scala.io.StdIn.readLine()
-    ok = ln != null
+    var ok = true
 
-    if (ln == "exit") System.exit(0)
-    if (ok) {
-      runQuery(ln)
+    while (ok) {
+      val ln = scala.io.StdIn.readLine()
+      ok = ln != null
+
+      if (ok) {
+        runQuery(ln)
+      }
     }
   }
 
